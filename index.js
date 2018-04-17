aef319554e63fe5077407b6fefd7469bd919af73
metallic <metallicbai@gmail.com>
2018-04-17 23:36:10 +0800

    modify

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const koa = require('koa');
+const path = require('path');
+
 
 const app = new koa();
+const static = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const router = require('./controller/router');
+const views = require('koa-views');
+
+// 设置静态服务
+app.use(static(path.join(__dirname,'./static'),{
+    extensions: ['html']
+}))
+
+// 解析post请求
+app.use(bodyParser());
+
+// 设置路由
+app.use(router.routes()).use(router.allowedMethods());
 
 
-app.use(async (ctx, next) => {
-    await next();
-    ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
+
+
+
+app.listen(3001,() => {
+    console.log('服务开始执行 http://localhost:3001')
 })
-
-
-app.listen(80)
-
-console.log('服务开始执行')
